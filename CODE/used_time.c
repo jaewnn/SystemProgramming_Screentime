2fbe3c3c2ee11c83c7b118a0f5b7a3311d6e7e40
miiniipark <miiniipark@gmail.com>
2023-05-11 16:25:02 +0900

    Add fuction get_user_name

--- CODE/used_time.c
+++ CODE/used_time.c
@@ -1,3 +1,12 @@
+#include <pwd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#ifdef DEBUG
+#include <stdio.h>
+#endif
+
 typedef struct {
     int start_time;
     char name[32];
@@ -8,3 +17,20 @@ typedef struct {
     int usage_time;
     char name[32];
 } name_usage_time;
+
+void get_user_name();
+
+char user[32];
+
+void get_user_name() {
+    uid_t uid = getegid();
+    struct passwd* pwd = getpwuid(uid);
+    strcpy(user, pwd->pw_name);
+}
+
+#ifdef DEBUG
+int main() {
+    get_user_name();
+    puts(user);
+}
+#endif
